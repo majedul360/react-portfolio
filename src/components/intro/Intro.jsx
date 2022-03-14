f11f67b961bb47c955afe7e38ef7a1f1c45cf0ff
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-03-14 13:50:23 +0600

    intro section done

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useRef } from "react";
+import { init } from "ityped";
 import "./intro.scss";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 const Intro = () => {
-  return <div className="intro" id="intro"></div>;
+  const animText = useRef();
+  useEffect(() => {
+    init(animText.current, {
+      showCursor: true,
+      typeSpeed: 250,
+      backSpeed: 200,
+      strings: ["Use with React.js!", "Yeah!"],
+    });
+  }, []);
+  return (
+    <div className="intro" id="intro">
+      <div className="container">
+        <div className="box">
+          <div className="image">
+            <img src="images/man.png" />
+          </div>
+        </div>
+        <div className="box">
+          <h2>hi there, i'm</h2>
+          <h1>majedul islam</h1>
+          <h3>
+            freelancer <span ref={animText}></span>
+          </h3>
+          <a href="#portfolio">
+            <FontAwesomeIcon className="arrow" icon={faAngleDown} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Intro;
