acd509ea1703656f491c7954f1542976ff7a8d17
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-03-15 21:19:39 +0600

    contact section done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,41 @@
 import "./contact.scss";
-
+import { useState, useRef } from "react/cjs/react.development";
 const Contact = () => {
-  return <div className="contact" id="contact"></div>;
+  const email = useRef();
+  const message = useRef();
+  const [isSuccessfulMessage, setIsSuccessfulMessage] = useState("");
+  const formHandler = (e) => {
+    e.preventDefault();
+    email.current.value && message.current.value
+      ? setIsSuccessfulMessage("Thanks, I'll reply ASAP!")
+      : setIsSuccessfulMessage("Please fill up email and message field!");
+
+    email.current.value = "";
+    message.current.value = "";
+  };
+  return (
+    <div className="contact" id="contact">
+      <div className="container">
+        <div className="box">
+          <img src="images/shake.svg" alt="" />
+        </div>
+        <div className="box">
+          <h3>contact</h3>
+          <form action="" onSubmit={formHandler}>
+            <input type="email" placeholder="email" ref={email} />
+            <textarea
+              cols="30"
+              rows="5"
+              placeholder="enter your message"
+              ref={message}
+            ></textarea>
+            <button type="summit">send message</button>
+          </form>
+          <p>{isSuccessfulMessage}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
